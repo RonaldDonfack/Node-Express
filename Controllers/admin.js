3e865b1c2ee0ddfeb0ff8b9ca33282edb1e84c16
RonaldDonfack <donfackronald13@gmail.com>
2024-07-10 15:49:36 +0100

    Done wwith the Editing functionality

--- Controllers/admin.js
+++ Controllers/admin.js
@@ -3,14 +3,22 @@ const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('admin/products', 
-            { prods: products, pageTitle: "All product", path: '/admin/products' });
+        res.render('admin/products',
+            {
+                prods: products,
+                pageTitle: "All product",
+                path: '/admin/products'
+            });
 
     });
 }
 exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product',
-         { pageTitle: 'Add-Product', path: '/admin/add-product' })
+    res.render('admin/edit-product',
+        {
+            pageTitle: 'Add-Product',
+            path: '/admin/add-product',
+            editing: false
+        })
 };
 
 exports.postProduct = (req, res, next) => {
@@ -19,7 +27,38 @@ exports.postProduct = (req, res, next) => {
     const price = req.body.price;
     const description = req.body.description;
 
-    const product = new Product(title, imageUrl, price, description);
+    const product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect('/');
 }
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const productId = req.params.productId;
+    Product.findById(productId, product => {
+        if (!product)
+            return res.redirect('/');
+        res.render('admin/edit-product',
+            {
+                pageTitle: 'edit-Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            })
+    })
+};
+exports.postEditProduct = (req, res, next) => {
+    const prodId = req.body.productId;
+    const updatedTitle = req.body.title;
+    const updatedPrice = req.body.price;
+    const updatedImageUrl = req.body.imageUrl;
+    const updatedDescription = req.body.description;
+
+    const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedPrice, updatedDescription);
+    updatedProduct.save();
+    res.redirect('/admin/products')
+
+}
+
